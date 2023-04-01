cb4027d9ec7a0c2becc86b4efea92c0c0135380a
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2023-04-01 11:47:23 -0300

    fix: das rotas de cliente

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,22 @@
 import express from 'express';
-import { clienteRouter } from './routes';
+import connectDB from './database';
+import { clienteRouter } from './cliente-routes';
 
 const app = express();
 app.use(express.json());
 
 // Adiciona as rotas do ClienteController ao servidor
-app.use('/clientes', clienteRouter);
+app.use(clienteRouter);
 
 // Inicia o servidor
 app.listen(3000, () => console.log('Servidor iniciado na porta 3000.'));
+
+connectDB()
+    .then(() => {
+        console.log('Database connected successfully');
+    })
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
+
