91e275bf5e862b028bb0c061c9c1b17ed6e2edf4
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2023-04-01 12:18:17 -0300

    organizando controller

--- src/controller/cliente-controller.ts
+++ src/controller/cliente-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { ClienteUseCase } from '../usecase/cliente/cliente-usecase';
-import { ICliente } from '../entity/cliente';
+import { Cliente } from '../entity/cliente';
 import { ClienteRepository } from '../repository/cliente-repository';
 
 export class ClienteController {
@@ -26,36 +26,42 @@ export class ClienteController {
 
   async consultarPorId(req: Request, res: Response) {
     try {
-      const cliente = await this.clienteUseCase.consultarPorId(req.params.id);
-
-      if (!cliente) {
-        return res.status(404).json({ message: 'Cliente não encontrado' });
+      const resposta = await this.clienteUseCase.consultarPorId(req.params.id);
+      if (!resposta) {
+        return res.status(404).json({ message: 'Id não encontrado' });
+      }
+      if (resposta instanceof Error) {
+        console.log(resposta.message)
+        return res.status(400).json({ message: resposta.message });
       }
 
-      return res.status(200).json(cliente);
+      return res.status(200).json(resposta);
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ message: 'Erro ao consultar cliente' });
+      return res.status(500).json({ message: 'Erro ao pesquisar' });
     }
   }
 
   async consultarPorEmail(req: Request, res: Response) {
     try {
-      const cliente = await this.clienteUseCase.consultarPorEmail(req.params.email);
-
-      if (!cliente) {
-        return res.status(404).json({ message: 'Cliente não encontrado' });
+      const resposta = await this.clienteUseCase.consultarPorEmail(req.params.email);
+      if (!resposta) {
+        return res.status(404).json({ message: 'Email não encontrado' });
+      }
+      if (resposta instanceof Error) {
+        console.log(resposta.message)
+        return res.status(400).json({ message: resposta.message });
       }
 
-      return res.status(200).json(cliente);
+      return res.status(200).json(resposta);
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ message: 'Erro ao consultar cliente' });
+      return res.status(500).json({ message: 'Erro ao pesquisar' });
     }
   }
 
   async adicionarCliente(req: Request, res: Response) {
-    const cliente = req.body as ICliente;
+    const cliente = req.body as Cliente;
 
     const resposta = await this.clienteUseCase.adicionarCliente(cliente);
     if (resposta instanceof Error) {
@@ -63,14 +69,21 @@ export class ClienteController {
       return res.status(400).json({ message: resposta.message });
     }
 
-    const clienteAdicionado = resposta as ICliente;
+    const clienteAdicionado = resposta as Cliente;
     return res.status(201).json(clienteAdicionado);
   }
 
   async atualizarCliente(req: Request, res: Response) {
     try {
-      const clienteAtualizado = req.body as ICliente;
+      const clienteAtualizado = req.body as Cliente;
+      console.log("clienteAtualizado", clienteAtualizado);
+      if (!clienteAtualizado.id) {
+        return res.status(400).json({ message: 'Id não informado' });
+      }
       const cliente = await this.clienteUseCase.atualizarCliente(clienteAtualizado);
+      if (cliente instanceof Error) {
+        return res.status(400).json({ message: cliente.message });
+      }
       return res.status(200).json(cliente);
     } catch (error) {
       console.error(error);
@@ -81,8 +94,14 @@ export class ClienteController {
   async deletarCliente(req: Request, res: Response) {
     try {
       const id = req.params.id;
-      await this.clienteUseCase.deletarCliente(id);
-      return res.status(204).send();
+      const retorno = await this.clienteUseCase.deletarCliente(id);
+      if (retorno instanceof Error) {
+        return res.status(400).json({ message: retorno.message });
+      }
+      if (!retorno) {
+        return res.status(404).json({ message: 'Id não encontrado' });
+      }
+      return res.status(204).send('Apagado com sucesso').end();
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: 'Erro ao deletar cliente' });
